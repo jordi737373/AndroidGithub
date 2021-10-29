731a9d20a026da9c7da104653fdcaed9cec506ae
jordi737373 <66683361+jordi737373@users.noreply.github.com>
2021-10-29 10:57:11 +0200

    update four files

--- app/src/main/java/com/rams/jordi/androidgithub/MainActivity.kt
+++ app/src/main/java/com/rams/jordi/androidgithub/MainActivity.kt
@@ -2,10 +2,23 @@ package com.rams.jordi.androidgithub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var text:TextView
+    private lateinit var btn:Button
+    private lateinit var image:ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        text=findViewById(R.id.textView)
+        image=findViewById(R.id.imageView)
+        btn=findViewById(R.id.button)
+
     }
 }
